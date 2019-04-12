69790130e52fc7bb1b8c6d0c54b5a4baefb0010f
shirano2 <shirano2@naver.com>
2019-04-11 23:35:38 -0500

    finished

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ var mysql = require("mysql");
 	
 var connection = mysql.createConnection({
   host: "localhost",
-  port: 3306,
+  port: 3307,
   user: "root",
   password: "1234",
   database: "burgers_db"
@@ -14,7 +14,7 @@ connection.connect(function(err) {
     console.error("error connecting: " + err.stack);
     return;
   }
-  console.log("Could you give me " + connection.threadId+" dollars?");
+  console.log("Can you give me " + connection.threadId+" dollars?");
 });
 
 module.exports=connection;
