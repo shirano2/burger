028fc51680881dd9351d8e070c8d0844897e52b5
shirano2 <shirano2@naver.com>
2019-04-12 14:24:50 -0500

    connection changed

--- config/connection.js
+++ config/connection.js
@@ -8,6 +8,7 @@ if(process.env.JAWSDB_URL) {
   connection=mysql.createConnection({
     host: "localhost",
     user: "root",
+    port: 3306,
     password: "1234",
     database: "burgers_db"
   })
